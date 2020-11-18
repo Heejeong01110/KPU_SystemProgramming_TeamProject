aa3c24056e2834254bc281b78a4bb1ee02acebcd
GEUP <knmj789@gmail.com>
2020-11-18 21:04:20 +0900

    파일 입출력 작업1 쓰레드 만들기

--- IOModuleClient.c
+++ IOModuleClient.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<pthread.h>
 
+#define THREAD_NUM 3
+
+void * reader(void * i)
+{
+	int n = (int)i;
+	printf("%d",n);
+	pthread_exit(0);
+}
 int main(void)
 {
+	pthread_t th[THREAD_NUM];
+	
+	for(int i = 0 ; i<THREAD_NUM; i++)
+	{
+		pthread_create(&th[i],NULL,reader,(void *)i);
+	}
+
+	pthread_exit(0);
 }
--- IOModuleServer.c
+++ IOModuleServer.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
+#define THREAD_NUM 6
+
+void * decryption(void * num)
+{
+	int n = (int)num;
+	printf("%d",n);
+	pthread_exit(0);
+}
 int main(void)
 {
+	pthread_t th[THREAD_NUM];
+
+	for(int i = 0;i<THREAD_NUM;i++)
+	{
+		pthread_create(&th[i],NULL,decryption,(void *)i);
+	}
+
+	
+	for(int i = 0;i<THREAD_NUM;i++)
+	{
+	}
+
+	pthread_exit(0);	
 }
