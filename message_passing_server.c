b3f2394bc7f388f0a64cbd968d6c0ea38ed1ed1b
Minu <rlaalsdn160@naver.com>
2020-11-24 17:09:06 +0900

    메시지 패싱 ver.1
    code.txt 파일을 클라이언트 측에서 쓰레드로 읽어서 server측으로 보내고 server 측에서 받은 buf를 확인

--- message_passing_server.c
+++ message_passing_server.c
@@ -13,31 +13,58 @@
 #include <time.h>
 
 #define MSG_SIZE 80
-
+#define BUF_SIZE 4096
+#define FILENAMESIZE 255
+#define null NULL
+#define THREADPERWORK 3
 key_t key_id, key_id2;  // , key_id3
 int clientCnt = 0;
-void* filerecv(int i/*int threadnum*/);
+void* filerecv(void * arg);
 pthread_t thread[3];
 
 
-typedef struct tTHREAD
+/*typedef struct tTHREAD
 {
 	char ddmsg[MSG_SIZE];
 	int cCnt;
 }THREAD;
+*/
+struct threadArg{
+	char fifoFileName[FILENAMESIZE];
+	long long fileSize;
+	int number;
+};
 
-THREAD recvbuf[3];
-THREAD sendbuf[3];
-int sender_id;
+void error_handler(char * linkFileName){
+  //  unlink(linkFileName);
+     perror("handler err\n");
+    exit(1);
+}
+
+void requestPasing(char * request[3],char buf[])
+{
+    int cnt=0;
+    request[0] = &buf[0];
+    for(int i = 0;i<BUF_SIZE;i++){
+        if(cnt>2)
+	    break;
+	if(buf[i]==' '){
+		buf[i]='\0';
+		cnt++;
+		request[cnt]=&buf[i+1];
+	}
+    }
+}
+//THREAD recvbuf[3];
+//THREAD sendbuf[3];
+//int sender_id;
 //sem_t threadrock;
 
 /*void make_Fifo() {
-
 	if (mkfifo("./managefifo", 0666) == -1) { //fifo init
 		printf("fail to make fifo manage()\n");
 		exit(1);
 	}
-
 }*/
 /*
 void open_Fifo() {
@@ -64,23 +91,33 @@ void rcv_Data() {
 //server
 int main()
 {
-	char msg[MSG_SIZE];
-	char cnt[MSG_SIZE];
-	
+	char buf[BUF_SIZE];
+    int protocol;
+    char * request[3];
+	//int readlen=0;
+	struct threadArg * argument;
+
 	key_id = msgget((key_t)60041, IPC_CREAT | 0666); //send queue
 	key_id2 = msgget((key_t)60042, IPC_CREAT | 0666); //recv queue
 	//key_id3= msgget((key_t)60043, IPC_CREAT|0666); //result queue
 
 	printf("시작 전\n");
 
-	for (int i = 0; i < 3; i++) {
-		pthread_create(&thread[i], NULL, (void*)filerecv,(int *)i/*, (void*)& recvbuf[i]*/);
+    requestPasing(request,buf);
+	for (int i = 0; i < THREADPERWORK; i++) {
+        argument = (struct threadArg *)malloc(sizeof(struct threadArg));
+		sprintf(argument->fifoFileName,"./%sFIFO%d",request[1],i+1);
+		argument->number=i+1;
+		argument->fileSize=atoll(request[2]);
+		pthread_create(&thread[i], NULL, (void*)filerecv, (void *)argument/*, (void*)& recvbuf[i]*/);
 	}
+    memset(buf,0x00,BUF_SIZE);
 
-	for (int i = 0; i <= 3; i++)
+    pthread_exit(0);
+	/*for (int i = 0; i <= THREADPERWORK; i++)
 	{
 		pthread_join(thread[i], NULL);
-	}
+	}*/
 	//sem_destroy(&threadrock);
 
 	//unlink("./managefifo");
@@ -89,33 +126,40 @@ int main()
 		printf("msgctl failed\n");
 		exit(1);
 	}
-	
+
 	return 0;
 }
 
-void* filerecv(int i/*int threadnum*/) {
+void* filerecv(void * arg/*int threadnum*/) {
 
 	//sender_id = getpid();
+    int fd;
+	char buf[BUF_SIZE];
+	char tempFileName[FILENAMESIZE];
+	int tempfd;
+	/*매개변수 저장*/
+	struct threadArg * argument = (struct threadArg*)arg;
 
-	if (msgrcv(key_id2, (void*)&recvbuf[i], sizeof(struct tTHREAD), 0, 0) == -1) {
+	if (msgrcv(key_id2, (void*)& buf, BUF_SIZE, 0, 0) == -1) {
 		perror("msgrcv error");
 		exit(1);
 	}
 	else {
 		printf("receive success\n");
-		printf("%d. %s\n", recvbuf[i].cCnt, recvbuf[i].ddmsg);
+		printf("%s\n", buf);
 
-		sendbuf[i].cCnt = recvbuf[i].cCnt;
+		/*sendbuf[i].cCnt = recvbuf[i].cCnt;
 		//sendbuf[n].ddmsg = recvbuf[n].ddmsg
 		strcpy(sendbuf[i].ddmsg, recvbuf[i].ddmsg);
 		//sendbuf[i].ddmsg = sendbuf[i].ddmsg - 32;
-		
+
 		if (msgsnd(key_id, (void*)& sendbuf[i], sizeof(struct tTHREAD), 0) == -1) { //fifo rw
 			printf("fail to call msgsnd()\n");
 			exit(1);
 		}
 		printf("send success\n");
 		printf("%d. %s\n", sendbuf[i].cCnt, sendbuf[i].ddmsg);
+        */
 	}
-	
+
 }
