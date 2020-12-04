f099be3f672060a3b8c87484301971a77f0d2aa9
Heejeong01110 <123rkdrkd@naver.com>
2020-12-05 05:26:17 +0900

    shared memory 완성
    shared memory 기능구현 끝.
    decoding에서 code.txt에 %가 들어있는 경우에 문자로 인식하지 않는 문제가 있어서 한글파일로 테스트해보니 잘됨

--- sharedClient.c
+++ sharedClient.c
@@ -15,56 +15,47 @@
 #include <sys/stat.h>
 
 #define  KEY_NUM   60043
-#define  MEM_SIZE  4096
 #define THREADNUM 3
-#define BUF_SIZE 4096
 #define MAX_SIZE 4096
-#define T 5
 #define FILENAMESIZE 255
-#define BUF_SIZE 4096
 #define null NULL
 
-union semun
-{
-    int val;
-    struct semid_ds *buf;
-    unsigned short int *array;
-};
+
 
 void* filesend(void* th);
 pthread_t thread[3];
 long mypid; /*main에서 한 번 write하고 쓰레드에서는 읽기만 함으로 동기화 필요없음 */
 struct stat sb; /*code.txt 파일의 상태를 저장함, 한 번 저장되면 읽기만 함으로 동기화 필요없음 */
-long long fileSize;
-int codeFd; /*code.txt 파일의 파일디스크립터*/
+long long afileSize;
 pthread_cond_t printer1;
 pthread_cond_t printer2;
 pthread_cond_t printer3;
 pthread_mutex_t printlock;
 int endNum;
 
-static int SharedMemoryManageInit(int kNum);
-static int SharedMemoryInit(int kNum);
-static int SharedMemoryWrite(int shmid,char *sMemory, int size,int i);
-static int SharedMemoryManageWrite(int shmid,char *sMemory, int size);
-static int SharedMemoryRead(int shmid,char *sMemory,int i);
+
+static int SharedMemoryInit(int kNum,long long fileSize);
+static int SharedMemoryWrite(int shmid,char *sMemory);
+static int SharedMemoryRead(int shmid,char *sMemory);
 static int SharedMemoryFree(int shmid);
 
-union semun sem_union;
-    int sem_num=1;
-    int sem_id;
-    struct sembuf mysem_open  = {0, -1, SEM_UNDO}; // 세마포어 얻기
-struct sembuf mysem_close = {0, 1, SEM_UNDO};  // 세마포어 돌려주
+void error_handler(char * FileName){
+    if(FileName!=null)
+        unlink(FileName);
+	pthread_exit(0);
+}
+struct threadArg{
+    long long fileSize; //파일 사이즈
+	int cCnt; //클라이언트 수
+};
 
-void* filesend(void* th);
-pthread_t thread[3];
 
 int main() {
-
-	char buf[BUF_SIZE];
+    struct threadArg * argument;
+	char buf[MAX_SIZE];
     int protocol;
 
-	memset(buf,0x00,BUF_SIZE);
+	memset(buf,0x00,MAX_SIZE);
 	pthread_cond_init(&printer1,NULL);
 	pthread_cond_init(&printer2,NULL);
 	pthread_cond_init(&printer3,NULL);
@@ -77,25 +68,37 @@ int main() {
     
     /*2. managefifo 파일에 "request {mypid} {파일 크기}"를 write */
     /*code.txt의 상태 읽기*/
-    if (stat("./code.txt", &sb) == -1){ 
+    if (stat("./codeqq.txt", &sb) == -1){ 
         printf("fail to call stat(./code,txt)\n");
         exit(1);
-    }   
-    fileSize = (long long)sb.st_size;
+    }
+
+    afileSize = (long long)sb.st_size;
     mypid = (long)getpid();
-    sprintf(buf,"request %ld %lld \n",mypid,fileSize);
-	printf("strlen(buf) = %d\n",strlen(buf));
+    sprintf(buf,"request %ld %lld \n",mypid,afileSize);
+
+    long long fileS = afileSize/3 + 1;
+    
+
+
     if(write(protocol, buf, strlen(buf)) < 0 ){ 
         printf("fail to call write(protocol,request)\n");
         exit(1);
     }
+
 	write(1,buf,strlen(buf));
     close(protocol);
+
+
     /*4. 3개의 쓰레드를 할당*/
     for(int i=0;i<THREADNUM;i++){
-	//실패 시 에러처리 추가 
-        pthread_create(&thread[i],NULL,(void *)filesend, (void *)(i+1));
+	    //실패 시 에러처리 추가 
+        argument = (struct threadArg *)malloc(sizeof(struct threadArg));
+        argument->fileSize = fileS;
+        argument->cCnt=i+1;
+        pthread_create(&thread[i],NULL,(void *)filesend, (void *)argument);
     }
+
 
     for(int i=0;i<THREADNUM;i++){
 	//실패 시 에러처리 추가
@@ -105,31 +108,32 @@ int main() {
     pthread_exit(1);
 }
 
-void* filesend(void* arg){ 
+    
 
+void* filesend(void* arg){ 
+    struct threadArg * argument = (struct threadArg*)arg;
+	int n = argument->cCnt; //스레드
+    long long fileSize = argument->fileSize;
     
     //int threadShmid = 10 + (int)arg;//10을 서버에서 받아오게 수정
-    int n = (int)arg;
-    char recvbuf[MEM_SIZE];
-	char sendbuf[MEM_SIZE];
+
+    char recvbuf[fileSize];
+	char sendbuf[fileSize];
     int threadId = (int)mypid;
-    int cCnt = (int)arg; //스레드
+    int cCnt = n; //스레드
     int tshmId = threadId*100 + cCnt*10;
     char linkFileName[FILENAMESIZE];
     int linkFd;
-	char buf[BUF_SIZE];
+	char buf[fileSize];
 	int readlen;
-	int readFlag = 0 ;
     long long rwpointer; /* read write pointer */
     fflush(stdout);
     
     /*3. 각 쓰레드는 1개의 shm 생성.*/
-    int stoctid = SharedMemoryInit(tshmId+1);
-    int ctostid = SharedMemoryInit(tshmId+2);
-    printf("thread%d stoc %d\n",cCnt,stoctid);
-    fflush(stdout);
+    int stoctid = SharedMemoryInit(tshmId+1,fileSize);
+    int ctostid = SharedMemoryInit(tshmId+2,fileSize);
 
-        /*5client의 각 쓰레드는 "code.txt"파일을 3분할하여 병행적으로 읽은 후 "{자신의 PID}FIFO{n}2ser" 파일에 writelock를 건 후 write한다.*/
+    /*5client의 각 쓰레드는 "code.txt"파일을 3분할하여 병행적으로 읽은 후 "{자신의 PID}FIFO{n}2ser" 파일에 writelock를 건 후 write한다.*/
     /* 파일을 병행적으로 읽는 방법 
 	1. link()를 통해 링크파일을 만든다(링크파일은 read write pointer를 공유하지 않지만 원본 파일의 내용은 공유하기 때문에 파일을 병행적으로 읽는 것에 적절하다)
 	2. 링크파일을 open한다.
@@ -137,98 +141,65 @@ void* filesend(void* arg){
     */
     
     sprintf(linkFileName,"codelink%ld_%d",mypid,n);
-    if(link("code.txt",linkFileName)<0){
-        printf("fail to call link(code.txt,%s)\n",linkFileName);
-        //error_handler(linkFileName);
+    if(link("codeqq.txt",linkFileName)<0){
+        printf("fail to call link(codeqq.txt,%s)\n",linkFileName);
+        error_handler(linkFileName);
         exit(0);
     }
     
     
     if((linkFd=open(linkFileName,O_RDONLY))<0){
         printf("fail to call open(%s)\n",linkFileName);
-        //error_handler(linkFileName);
+        error_handler(linkFileName);
         exit(0);
     }
-    rwpointer = (fileSize/THREADNUM)*(n-1);
+
+    rwpointer = (fileSize)*(n-1);
     if(lseek(linkFd,rwpointer,SEEK_SET)<0){
         printf("fail to call lseek(%s)\n",linkFileName);
-        //error_handler(linkFileName);
+        error_handler(linkFileName);
         exit(0);
     }
 
 	int totalfilereadlen = 0; //파일 사이즈 알 필요없음
-    int i=0;
-    if((readlen=read(linkFd,buf,fileSize/THREADNUM))<0){
-            printf("fail to call read()\n");
-            //error_handler(linkFileName);
-            exit(0);
-        }
-
-    
-    SharedMemoryWrite(ctostid,buf,fileSize/THREADNUM,i);
-    printf("thread%d first write  버퍼 : %s",threadId,buf);
-    fflush(stdout);
-    /*
-    for(i = 0 ;i < (fileSize/THREADNUM)/BUF_SIZE;i++){
-        if((readlen=read(linkFd,buf,BUF_SIZE))<0){
-            printf("fail to call read()\n");
-            //error_handler(linkFileName);
-            exit(0);
-        }
-        //message send
-        SharedMemoryWrite(ctostid,buf,BUF_SIZE,i);
-	    
-	totalfilereadlen += readlen;
-    }
-    if((readlen=read(linkFd,buf,(fileSize/THREADNUM)%BUF_SIZE))<0){ 
+    if((readlen=read(linkFd,buf,fileSize))<0){
         printf("fail to call read()\n");
-        //error_handler(linkFileName);
+        error_handler(linkFileName);
         exit(0);
     }
-    printf("버퍼 test2 : %s\n",buf);
-    fflush(stdout);
 
-	SharedMemoryWrite(ctostid,buf,fileSize/THREADNUM,i);
-    */
+    SharedMemoryWrite(ctostid,buf);
+ 
 
-
-    //8. client의 1번 쓰레드는 "{자신의 PID}FIFO12cli"의 내용을 읽고 출력한다.
-	//9. 8이 끝나면 client의 2번 쓰레드는 "{자신의 PID}FIFO22cli"의 내용을 읽고 출력한다.
-	//10. 9가 끝나면 client의 3번 쓰레드는 "{자신의 PID}FIFO32cli"의 내용을 읽고 출력한다.
-    //sleep(10);
 	pthread_mutex_lock(&printlock);
 	if(n==1)
 		printf("\n\nThread1 Print Start\n");
+        fflush(stdout);
 	if((n==2)){
 		if(endNum!=1)
 			pthread_cond_wait(&printer2,&printlock);
 		printf("\n\nThread2 Print Start\n");
+        fflush(stdout);
 	}
 	else if((n==3)){
 		if((endNum!=2))
 			pthread_cond_wait(&printer3,&printlock);
 		printf("\n\nThread3 Print Start\n");
+        fflush(stdout);
 	}
 	
+    
 	int totallen=0;
-    i=0;
-
-    /*
-    for(i = 0 ;i < (fileSize/THREADNUM)/BUF_SIZE;i++){
-        SharedMemoryRead(stoctid,buf,i);
-        totallen+=write(1,buf,readlen);
-    }*/
+    printf("%d - %d test3\n",tshmId,cCnt);
+    fflush(stdout);
     
     while(1){
-        if(SharedMemoryRead(stoctid,buf,i)){
+        if(SharedMemoryRead(stoctid,buf)){
             break;
         }
     }
-    //SharedMemoryRead(stoctid,buf,i)
     
     totallen+=write(1,buf,readlen);
-    
-
 	fflush(stdout);
 	if(n==1){
 		endNum=1;
@@ -239,29 +210,20 @@ void* filesend(void* arg){
 		pthread_cond_signal(&printer3);
 	}
 	pthread_mutex_unlock(&printlock);
-	printf("filesize = %d Thread %d, fileread: %d byte, rcv: %d byte\n",fileSize,n,totalfilereadlen, totallen);
+
+
+	printf("\nfilesize = %lld Thread %d, fileread: %d byte, rcv: %d byte\n",afileSize,n,totalfilereadlen, totallen);
+    SharedMemoryFree(stoctid);
+    SharedMemoryFree(ctostid);
 	unlink(linkFileName);
-
-
-}
-
-static int SharedMemoryManageInit(int kNum)
-{
-    int shmid; 
-    if((shmid = shmget((key_t)kNum, 0, 0))==-1)
-    {
-        //perror("write Shmat failed");
-    }else{
-        return shmid;
-    }
-    return 0;
 }
 
 
-static int SharedMemoryInit(int kNum)
+
+static int SharedMemoryInit(int kNum,long long fileSize)
 {
     int shmid;
-    if((shmid = shmget((key_t)kNum, MEM_SIZE, IPC_CREAT| IPC_EXCL | 0666)) == -1) {
+    if((shmid = shmget((key_t)kNum, fileSize, IPC_CREAT| IPC_EXCL | 0666)) == -1) {
         //새로 만드는 경우
         //서버 측에서 이미 공유메모리 생성
         if((shmid = shmget((key_t)kNum, 0, 0))==-1)
@@ -270,90 +232,50 @@ static int SharedMemoryInit(int kNum)
             return shmid;
         }
     }
-    SharedMemoryWrite(shmid,"0",1,1);
+    SharedMemoryWrite(shmid,"0");
     //클에서 만듦
-    //printf("클 생성해서 연결 성공%d\n",kNum);  
     return shmid;
 }
 
-static int SharedMemoryManageWrite(int shmid, char *sMemory, int size)
+
+static int SharedMemoryWrite(int shmid, char *sMemory)
 {
     void *shmaddr;
-    char temp[MEM_SIZE];
-    char* recvM = temp;
-    //printf("before write :%s",sMemory);
-        if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){}
-        else{
+    if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){
 
-        if(semop(sem_id, &mysem_open, 1) == -1)
-        {
-        //perror("semop error ");
-        exit(0);
-        }
+    }else{
+        sprintf((char*)shmaddr,sMemory);
 
-        sprintf(recvM,shmaddr);
-
-        
-        
-        if(strcmp(recvM,"hello")==0){
-            semop(sem_id, &mysem_close, 1);
-
-            if(shmdt(shmaddr) == -1)
-            {
-                perror("close write Shmdt failed");
-            }
-            return 0;
-           
-        }else{
-            sprintf((char*)shmaddr,sMemory);
-            semop(sem_id, &mysem_close, 1);
-            if(shmdt(shmaddr) == -1)
-            {
-                perror("close write Shmdt failed");
-            }
-            return 1;
-        }
-        }
-}
-
-static int SharedMemoryWrite(int shmid, char *sMemory, int size,int i)
-{
-    void *shmaddr;
-    //printf("before write :%s",sMemory);
-        if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){}
-        else{
-     
-            sprintf((char*)shmaddr,sMemory);
-
-       
-            //printf("after write :%s",(char*)shmaddr);
-            if(shmdt(shmaddr) == -1)
-            {
-                perror("close write Shmdt failed");
-            }
+        if(shmdt(shmaddr) == -1){
+            perror("close write Shmdt failed");
             return 0;
         }
+        return 1;
+
+    }
     return 0;
 }
 
 
-static int SharedMemoryRead(int shmid,char *sMemory,int i)
+static int SharedMemoryRead(int shmid,char *sMemory)
 {
     void *shmaddr;
-    if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){}
-        else{
-            //printf("받은버퍼 :%s\n",(char*)shmaddr);
-            sprintf(sMemory,shmaddr);
-            if(sMemory[0]=='0'){
-                return 0;
-            }
-            //printf("받은 문자열 :%s\n",sMemory);
+    if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){
+    }
+    else{
+        sprintf(sMemory,shmaddr);
+        if(sMemory[0]=='0'){
+            return 0;
+        }else{
+            sprintf(shmaddr,'\0');
             if(shmdt(shmaddr) == -1)
             {
                 perror("close write Shmdt failed");
+                return 0;
             }
             return 1;
         }
+    }
     return 0;
 }
 
@@ -362,7 +284,7 @@ static int SharedMemoryFree(int shmid)
 {
     if(shmctl(shmid, IPC_RMID, 0) == -1) 
     {
-        //perror("Shmctl failed");
+        perror("Shmctl failed");
         return 1;
     }
     //printf("Shared memory end\n");
--- sharedServer.c
+++ sharedServer.c
@@ -15,25 +15,15 @@
 #include <signal.h>
 #include <sys/stat.h>
 
-#define  KEY_NUM   60043 /*manage 공유메모리 키값 */
-#define  MEM_SIZE  4096
-#define THREADNUM 3 /*스레드 수*/
+#define KEY_NUM   60043 /*manage 공유메모리 키값 */
+#define MAX_SIZE  4096
 #define THREADPERWORK 3
-#define BUF_SIZE 4096
-#define FILENAMESIZE 200
 #define null NULL
 
-union semun
-{
-    int val;
-    struct semid_ds *buf;
-    unsigned short int *array;
-};
 
 struct threadArg{
 	int tshmid; //스레드 순서
     long long fileSize; //파일 사이즈
-
 	int cCnt; //클라이언트 수
 };
 
@@ -41,52 +31,45 @@ void requestPasing(char * request[3],char buf[])
 {
     int cnt=0;
     request[0] = &buf[0];
-    for(int i = 0;i<BUF_SIZE;i++){
+    for(int i = 0;i<MAX_SIZE;i++){
         if(cnt>2)
-	    break;
-	if(buf[i]==' '){
-		buf[i]='\0';
-		cnt++;
-		request[cnt]=&buf[i+1];
-	}
+	        break;
+	    if(buf[i]==' '){
+		    buf[i]='\0';
+		    cnt++;
+		    request[cnt]=&buf[i+1];
+	    }
     }
 }
 void signalhandler(int sig) {
-	//error_handler("./managefifo",null,null,null);
+	unlink("./managefifo");
 	exit(0);
 }
 
 void decoding(char code[],int len)
 {
-	for(int i = 0 ; i<len;i++)
-	{
+	for(int i = 0 ; i<len;i++){
 		code[i] = code[i]-1;
 	}
 }
-static int SharedMemoryManageCreate(int kNum);
-static int SharedMemoryCreate(int kNum);
-static int SharedMemoryWrite(int shmid, char *sMemory, int size,int i);
-static int SharedMemoryRead(int shmid,char *sMemory,int i);
-static int SharedMemoryFree(int shmid);
-static int SharedMemoryManageRead(int shmid,char *sMemory);
+static int SharedMemoryCreate(int kNum,long long fileSize);
+static int SharedMemoryWrite(int shmid, char *sMemory);
+static int SharedMemoryRead(int shmid,char *sMemory);
 
-int clientCnt=0;
 void* filerecv(void* arg);
 pthread_t thread[3];
 
 int main(void){
-    char buf[BUF_SIZE];
+    char buf[MAX_SIZE];
     int protocol;
     char * request[3];
 	int readlen=0;
 	struct threadArg * argument;
-    //unlink("./managefifo");
-    printf("시작 전\n");
+    printf("서버를 시작합니다.\n");
     /*1. server가 managefifo file을 생성한다.*/
     if(mkfifo("./managefifo",0666) == -1){ //fifo init
         printf("fail to make fifo manage()\n");
         exit(0);
-        //error_handler("./managefifo",null,null,null);
     }
     signal(SIGINT, signalhandler);
     
@@ -95,159 +78,71 @@ int main(void){
     	if((protocol = open("./managefifo", O_RDONLY)) < 0){ //waiting in client
         	printf("fail to call open manage()\n");
             exit(0);
-        	//error_handler("./managefifo",null,null,null);
     	}
-        if((readlen=read(protocol, buf, BUF_SIZE)) < 0 ){ 
-		printf("fail to call read()");
-        exit(0);
-		//error_handler("./managefifo",null,null,null);
+        if((readlen=read(protocol, buf, MAX_SIZE)) < 0 ){ 
+		    printf("fail to call read()");
+            exit(0);
         }
-	if(readlen<7) /*읽은 값이 "request"의 길이인 7보다 작으면 while문 재시작*/
-		continue;
-	requestPasing(request,buf);
+	    if(readlen<7) /*읽은 값이 "request"의 길이인 7보다 작으면 while문 재시작*/
+		    continue;
+	    requestPasing(request,buf);
+
+        long long fileS = atoll(request[2])/3 + 1;
+        
 	
-	printf("%s %s %s 처리 시작\n",request[0],request[1],request[2]);
-	
-	for(int i = 0 ;i<THREADPERWORK;i++){
-		argument = (struct threadArg *)malloc(sizeof(struct threadArg));
-		argument->tshmid=atoi(request[1]);
-		argument->fileSize=atoll(request[2]);
-        argument->cCnt= i+1;
-		pthread_create(&thread[i],NULL,filerecv, (void *)argument); //실패 시 에러처리 추가
-		
-	}
-	memset(buf,0x00,BUF_SIZE);
-	close(protocol);
+	    for(int i = 0 ;i<THREADPERWORK;i++){
+		    argument = (struct threadArg *)malloc(sizeof(struct threadArg));
+		    argument->tshmid=atoi(request[1]);
+		    argument->fileSize=fileS;
+            argument->cCnt= i+1;
+		    pthread_create(&thread[i],NULL,filerecv, (void *)argument); //실패 시 에러처리 추가
+	    }
+	    memset(buf,0x00,MAX_SIZE);
+	    close(protocol);
     }
     unlink("./managefifo");
     pthread_exit(0);
 }
-/*
-struct threadArg{
-	int tshmid; //스레드 순서
-    long long fileSize; //파일 사이즈
-	int cCnt; //클라이언트 수
-};
-*/
 
 void* filerecv(void * arg){
     struct threadArg * argument = (struct threadArg*)arg;
-	int threadId = argument->cCnt; //스레드
+	int threadId = argument->tshmid; //프로세스
     long long fileSize = argument->fileSize;
-    int cCnt = argument->tshmid; //프로세스
+    int cCnt = argument->cCnt; //스레드 
 
-    int tshmId = cCnt*100 + threadId*10;
-    char recvbuf[MEM_SIZE];
-	char sendbuf[MEM_SIZE];
-
-    char tempFileName[FILENAMESIZE];
+    int tshmId = threadId*100 +cCnt*10;
+    
 	int tempfd;
-    char buf[BUF_SIZE];
+    char buf[fileSize];
 	int readlen;
 
-    sprintf(tempFileName,"./%dtemp.txt",tshmId);
-
 	/*3. 각 쓰레드는 1개의 shm 생성.*/
-    int stoctid = SharedMemoryCreate(tshmId+1);
-    int ctostid = SharedMemoryCreate(tshmId+2);
-    printf("thread%d stoc %d\n",threadId,stoctid);
-    fflush(stdout);
+    int stoctid = SharedMemoryCreate(tshmId+1,fileSize);
+    int ctostid = SharedMemoryCreate(tshmId+2,fileSize);
 
-    /*6. server의 각 쓰레드는 "{요청자 PID}FIFO{n}" 파일을 읽고 복호화하여 "{요청자 PID}FIFO{n}temp.txt"파일에 임시 저장한다.*/
-	
-	if((tempfd = open(tempFileName,O_RDWR|O_CREAT))<0){ 
-        	printf("fail to call open manage()\n");
-            exit(0);
-		    //error_handler("./managefifo",fifo2SerFileName,fifo2CliFileName,tempFileName);
-    	}
-	
     int i=0;
     
     //sleep(1);
     while(1){
-        if(SharedMemoryRead(ctostid,buf,i)){
+        if(SharedMemoryRead(ctostid,buf)){
             break;
         }
-        
     }
     
+    decoding(buf,fileSize);
     
-    
-    printf("thread%d first read  버퍼 : %s",threadId,buf);
-    fflush(stdout);
-
-    decoding(buf,fileSize/THREADNUM);
-    write(tempfd,buf,fileSize/THREADNUM);
-
-
-    /*
-	for(i = 0 ;i < (fileSize/THREADNUM)/BUF_SIZE;i++){
-        SharedMemoryRead(ctostid,buf,i);
-        printf("%s 성공\n",buf);
-        decoding(buf,BUF_SIZE);
-        write(tempfd,buf,BUF_SIZE);
-    }*/
-    
-    /* 7. server의 각 쓰레드는 "{요청자 PID}FIFO{n}temp.txt" 읽고 "{요청자 PID}FIFO{n}" 파일에 writelock를 건 후 write한다.*/
-	lseek(tempfd,0,SEEK_SET);
-	int totallen=0;
-    i=0;
-
-    read(tempfd,buf,fileSize/THREADNUM);
-	
-
-	/*
-    while((readlen=read(tempfd,buf,BUF_SIZE))>0){
-		SharedMemoryWrite(stoctid,buf,BUF_SIZE,i++); 
-		totallen+=readlen;
-		
-	}*/
-    printf("read temp file thread%d 버퍼: %s\n",threadId,buf);
-    fflush(stdout);
-
-    SharedMemoryWrite(stoctid,buf,fileSize/THREADNUM,i++); 
-
-    //SharedMemoryFree(tid);
+    SharedMemoryWrite(stoctid,buf); 
+    if(cCnt==3){
+        printf("...decoding 완료\n");
+        fflush(stdout);
+    }
     pthread_exit(0);
-
 }
 
-static int SharedMemoryManageCreate(int kNum) //manage는 무조건 서버에서 새로생성할 필요가 있는거같다
+static int SharedMemoryCreate(int kNum,long long fileSize) //성공시 shmid 리턴
 {
     int shmid;
-    if((shmid = shmget((key_t)kNum, MEM_SIZE, IPC_CREAT| IPC_EXCL | 0666)) == -1) {
-        //IPC_CREATE - key에 해당하는 메모리가 없으면 공유메모리를 생성한다.
-        //IPC_EXCL - 공유메모리가 있으면 실패로 반환 
-        //클라이언트 측에서 이미 생성
-        shmid = shmget((key_t)kNum, MEM_SIZE, IPC_CREAT| 0666);
-        if(shmid == -1)
-        {
-            printf("error1");
-            return -1;
-        }
-        else
-        {   
-            SharedMemoryFree(shmid);
-            shmid = shmget(kNum, MEM_SIZE, IPC_CREAT| 0666);
-            if(shmid == -1)
-            {
-                printf("error2");
-                return 1;
-            }
-            return shmid;
-        }
-    }
-    return shmid;
-}
-
-
-
-
-
-static int SharedMemoryCreate(int kNum) //성공시 shmid 리턴
-{
-    int shmid;
-    if((shmid = shmget((key_t)kNum, MEM_SIZE, IPC_CREAT| IPC_EXCL | 0666)) == -1) {
+    if((shmid = shmget((key_t)kNum, fileSize, IPC_CREAT| IPC_EXCL | 0666)) == -1) {
         //IPC_CREATE - key에 해당하는 메모리가 없으면 공유메모리를 생성한다.
         //IPC_EXCL - 공유메모리가 있으면 실패로 반환 
         //클라이언트 측에서 이미 생성
@@ -256,81 +151,50 @@ static int SharedMemoryCreate(int kNum) //성공시 shmid 리턴
             //perror("write Shmat failed");
         }else{
             //printf("클측 생성 연결 성공%d\n",kNum);
-            
-
             return shmid;
         }
     }
-    SharedMemoryWrite(shmid,"0",1,1);
+    SharedMemoryWrite(shmid,"0");
     //서버측에서 정상적으로 만든경우
-    //printf("서버측 생성 연결 성공%d\n",kNum);
     return shmid;
 }
  
-static int SharedMemoryWrite(int shmid, char *sMemory, int size,int i)
+static int SharedMemoryWrite(int shmid, char *sMemory)
 {
     void *shmaddr;
     if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){
-        //연결 될때까지 무한루프
     }else{
-        //sprintf((char*)&shmaddr[BUF_SIZE*i],sMemory);
+        //sprintf((char*)&shmaddr[MAX_SIZE*i],sMemory);
         sprintf((char*)shmaddr,sMemory);
         //printf("보내진거 :%s\n",shmaddr);
         if(shmdt(shmaddr) == -1)
         {
-            printf("error3");
+            printf("error\n");
+            return 0;
         }
-        return 0;
+        return 1;
     }
     return 0;
 }
 
  
-static int SharedMemoryRead(int shmid,char *sMemory,int i)
+static int SharedMemoryRead(int shmid,char *sMemory)
 {
     void *shmaddr;
     if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){}
     else{
-        //sprintf(sMemory,&shmaddr[BUF_SIZE*i]);
-        
         sprintf(sMemory,shmaddr);
         if(sMemory[0]=='0'){
             return 0;
+        }else{
+            sprintf(shmaddr,'\0');
+            if(shmdt(shmaddr) == -1)
+            {
+                printf("error4");
+                return 0;
+            }
+            return 1;
         }
-        if(shmdt(shmaddr) == -1)
-        {
-            printf("error4");
-        }
-        return 1;
-    }
-    return 0;
-}
-
- 
-static int SharedMemoryFree(int shmid)
-{
-    if(shmctl(shmid, IPC_RMID, 0) == -1) 
-    {
-        //perror("Shmctl failed");
-        return 1;
-    }
-    //printf("Shared memory end\n");
-    return 0;
-}
-
-
-static int SharedMemoryManageRead(int shmid,char *sMemory)
-{
-    void *shmaddr;
-    if((shmaddr = shmat(shmid, (void *)0, 0)) == (void *)-1){}
-    else{
-        sprintf(sMemory,shmaddr);
-        sprintf(shmaddr,"\0");
-        if(shmdt(shmaddr) == -1)
-        {
-            printf("error4");
-        }
-        return 0;
     }
     return 0;
 }
