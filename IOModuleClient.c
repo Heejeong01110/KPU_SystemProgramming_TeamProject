560285880ffe9deb46576e4a035f2b078c3eda38
GEUP <knmj789@gmail.com>
2020-12-07 23:28:29 +0900

    1207

--- IOModuleClient.c
+++ IOModuleClient.c
@@ -30,24 +30,24 @@ pthread_cond_t printer3;
 pthread_mutex_t printlock;
 int endNum;
 /* 이 함수를 사용하면 에러처리를 위해 프로그램을 exit() 시키기 전에 FIFO파일이나 link파일을 삭제시켜줄 수 있다.*/
-void error_handler(void) {
+void file_remove(void) {
 	char fileName[FILENAMESIZE];
 	for (int n = 1; n <= 3; n++) {
-		sprintf(fileName, "./channel/%ldFIFO%d2ser", getpid(), n);
+		snprintf(fileName,FILENAMESIZE-1, "./channel/%ldFIFO%d2ser", getpid(), n);
 		remove(fileName);
-		sprintf(fileName, "./channel/%ldFIFO%d2cli", getpid(), n);
+		snprintf(fileName,FILENAMESIZE-1, "./channel/%ldFIFO%d2cli", getpid(), n);
 		remove(fileName);
-		sprintf(fileName, "./channel/%ldFIFO%d2temp.txt", getpid(), n);
+		snprintf(fileName,FILENAMESIZE-1, "./channel/%ldFIFO%d2temp.txt", getpid(), n);
 		remove(fileName);
-		sprintf(fileName, "./channel/codelink%d_%d", getpid(), n);
-		remove(fileName);
+		snprintf(fileName,FILENAMESIZE-1, "./channel/codelink%d_%d", getpid(), n);
+		unlink(fileName);
 		
 	}
-	exit(0);
+	
 }
 
 void signalhandler(int sig) {
-	error_handler();
+	file_remove();
 	exit(0);
 }
 
@@ -71,8 +71,7 @@ int main()
 	/*2. open managefifo file 만약 파일이 없다면 {T}시간 동안 스핀하며 대기한다.*/
 	//{T} 시간 측정 시작
 	while ((protocol = open("./managefifo", O_APPEND | O_WRONLY)) < 0) {
-		//{T} 시간을 넘었는지 확인하고 넘었다면 프로세스 종료 
-		printf("errno:%d",errno);
+		//{T} 시간을 넘었는지 확인하고 넘었다면 프로세스 종료
 	}
 	signal(SIGINT, signalhandler);
 	/*2. managefifo 파일에 "request {mypid} {파일 크기}"를 write */
@@ -83,11 +82,11 @@ int main()
 	}
 	fileSize = (long long)sb.st_size;
 	mypid = (long)getpid();
-	sprintf(buf, "request %ld %lld \n", mypid, fileSize);
+	snprintf(buf,FILENAMESIZE-1, "request %ld %lld \n", mypid, fileSize);
 	printf("strlen(buf) = %d\n", strlen(buf));
 	if (fcntl(protocol, F_SETLKW, &fifo_lock) == -1) {
 		printf("fail to call fcntl()\n");
-		error_handler();
+		exit(0);
 	}
 	if (write(protocol, buf, strlen(buf)) < 0) {
 		printf("fail to call write(protocol,request)\n");
@@ -96,7 +95,7 @@ int main()
 	fifo_lock.l_type = F_UNLCK;
 	if (fcntl(protocol, F_SETLK, &fifo_lock) == -1) {
 		printf("fail to call write()\n");
-		error_handler();
+		exit(0);
 	}
 	write(1, buf, strlen(buf));
 	close(protocol);
@@ -110,7 +109,7 @@ int main()
 		//실패 시 에러처리 추가
 		pthread_join(thread[i], NULL);
 	}
-	error_handler();
+	file_remove();
 	return 0;
 }
 
@@ -135,13 +134,13 @@ void* filesend(void* arg) {
 
 	long long rwpointer; /* read write pointer */
 	/*4. "{자신의 PID}FIFO1{n}2ser"와 "{자신 PID}FIFO{n}2cli"를 open한다*/
-	sprintf(fifo2SerFileName, "./channel/%ldFIFO%d2ser", mypid, n);
-	sprintf(fifo2CliFileName, "./channel/%ldFIFO%d2cli", mypid, n);
+	snprintf(fifo2SerFileName,FILENAMESIZE-1, "./channel/%ldFIFO%d2ser", mypid, n);
+	snprintf(fifo2CliFileName,FILENAMESIZE-1, "./channel/%ldFIFO%d2cli", mypid, n);
 	//{T} 시간 측정 시작
 	while ((fifo2Ser = open(fifo2SerFileName, O_WRONLY)) < 0) {
 		//{T} 시간을 넘었는지 확인하고 넘었다면 프로세스 종료 
 	}
-	while ((fifo2Cli = open(fifo2CliFileName, O_RDONLY)) < 0) {
+	while ((fifo2Cli = open(fifo2CliFileName, O_RDONLY|O_NONBLOCK)) < 0) {
 		//{T} 시간을 넘었는지 확인하고 넘었다면 프로세스 종료 
 	}
 
@@ -151,55 +150,56 @@ void* filesend(void* arg) {
 	2. 링크파일을 open한다.
 	3. lseek()를 통해 적절한 위치로 read write pointer 이동시킨다.
 	*/
-	sprintf(linkFileName, "./channel/codelink%ld_%d", mypid, n);
+	snprintf(linkFileName,FILENAMESIZE-1, "./channel/codelink%ld_%d", mypid, n);
 	if (link("code.txt", linkFileName) < 0) {
 		printf("fail to call link(code.txt,%s)\n", linkFileName);
-		error_handler();
+		exit(0);
 	}
 	if ((linkFd = open(linkFileName, O_RDONLY)) < 0) {
 		printf("fail to call open(%s)\n", linkFileName);
-		error_handler();
+		exit(0);
 	}
 	rwpointer = (fileSize / THREADNUM) * (n - 1);
 	if (lseek(linkFd, rwpointer, SEEK_SET) < 0) {
 		printf("fail to call lseek(%s)\n", linkFileName);
-		error_handler();
+		exit(0);
 	}
 
 
 	if (fcntl(fifo2Ser, F_SETLKW, &fifo_lock) == -1) {
 		printf("fail to call fcntl()\n");
-		error_handler();
+		exit(0);
 	}
 	int totalfilereadlen = 0;
 	for (int i = 0; i < (fileSize / THREADNUM) / BUF_SIZE; i++) {
 
 		if ((readlen = read(linkFd, buf, BUF_SIZE)) < 0) {
 			printf("fail to call read()\n");
-			error_handler();
+			exit(0);
 		}
 		if (write(fifo2Ser, buf, BUF_SIZE) < 0) {
 			printf("fail to call write()\n");
-			error_handler();
+			exit(0);
 		}
 		totalfilereadlen += readlen;
 	}
 
 	if ((readlen = read(linkFd, buf, (fileSize / THREADNUM) % BUF_SIZE)) < 0) {
 		printf("fail to call read()\n");
-		error_handler();
+		exit(0);
 	}
 
 	if (write(fifo2Ser, buf, (fileSize / THREADNUM) % BUF_SIZE) < 0) {
 		printf("fail to call read()\n");
-		error_handler();
+		exit(0);
 	}
 
 	totalfilereadlen += readlen;
+	printf("Thread %d, send to server to decoding: %d byte\n", n,totalfilereadlen);
 	fifo_lock.l_type = F_UNLCK;
 	if (fcntl(fifo2Ser, F_SETLK, &fifo_lock) == -1) {
 		printf("fail to call write()\n");
-		error_handler();
+		exit(0);
 	}
 
 	//8. client의 1번 쓰레드는 "{자신의 PID}FIFO12cli"의 내용을 읽고 출력한다.
@@ -221,9 +221,13 @@ void* filesend(void* arg) {
 	}
 
 	int totallen = 0;
-	while ((readlen = read(fifo2Cli, buf, BUF_SIZE)) > 0) {
-		totallen += write(1, buf, readlen);
+	int readflag = 0;
+	while (readflag == 0) {
+		while ((readlen = read(fifo2Cli, buf, BUF_SIZE)) > 0) {
+			totallen += write(1, buf, readlen);
+			readflag=1;
 
+		}
 	}
 	fflush(stdout);
 	if (n == 1) {
@@ -236,8 +240,6 @@ void* filesend(void* arg) {
 	}
 	pthread_mutex_unlock(&printlock);
 	printf("filesize = %d Thread %d, fileread: %d byte, rcv: %d byte\n", fileSize, n, totalfilereadlen, totallen);
-	unlink(linkFileName);
-
 }
 
 
--- IOModuleServer.c
+++ IOModuleServer.c
@@ -12,7 +12,7 @@
 #include <errno.h>
 
 #define BUF_SIZE 4096
-#define FILENAMESIZE 200
+#define FILENAMESIZE 255
 #define null NULL
 #define THREADPERWORK 3
 pthread_t thread[30000];
@@ -28,7 +28,7 @@ struct threadArg {
 
 
 /* 이 함수를 사용하면 에러처리를 위해 프로그램을 exit() 시키기 전에 FIFO파일이나 link파일을 삭제시켜줄 수 있다.*/
-void error_handler(char* FileName1, char* FileName2, char* FileName3, char* FileName4) {
+void file_remove(char* FileName1, char* FileName2, char* FileName3, char* FileName4) {
 	if (FileName1 != null)
 		unlink(FileName1);
 	if (FileName2 != null)
@@ -37,10 +37,10 @@ void error_handler(char* FileName1, char* FileName2, char* FileName3, char* File
 		unlink(FileName3);
 	if (FileName4 != null)
 		unlink(FileName4);
-	pthread_exit(0);
+	
 }
 void signalhandler(int sig) {
-	error_handler("./managefifo", null, null, null);
+	file_remove("./managefifo", null, null, null);
 	exit(0);
 }
 
@@ -91,20 +91,20 @@ int main()
 	if (mkfifo("./managefifo", 0666) == -1) { //fifo init
 		if(errno!=EEXIST){		
 			printf("fail to make fifo manage()\n");
-			error_handler("./managefifo", null, null, null);
+			exit(0);
 		}
 	}
 	signal(SIGINT, signalhandler);
 	if ((protocol = open("./managefifo", O_RDONLY,O_NONBLOCK)) < 0) { //waiting in client
 			printf("fail to call open manage()\n");
-			error_handler("./managefifo", null, null, null);
+			exit(0);
 	}
 	while (1) {
 		/*3. server는 managefifo에 있는 요청 메시지("request {자신의 PID} {파일 크기}")를 확인하면 응답을 위해 3개의 쓰레드를 할당하고*/
 		
 		if ((readlen = readRequest(protocol,buf)) < 0) {
 			printf("fail to call read()");
-			error_handler("./managefifo", null, null, null);
+			exit(0);
 		}
 		if (readlen < 7) /*읽은 값이 "request"의 길이인 7보다 작으면 while문 재시작*/{
 			memset(buf, 0x00, BUF_SIZE);
@@ -117,7 +117,7 @@ int main()
 
 		for (int i = 0; i < THREADPERWORK; i++) {
 			argument = (struct threadArg*)malloc(sizeof(struct threadArg));
-			sprintf(argument->fifoFileName, "./channel/%sFIFO%d", request[1], i + 1);
+			snprintf(argument->fifoFileName,FILENAMESIZE-1, "./channel/%sFIFO%d", request[1], i + 1);
 			argument->number = i + 1;
 			argument->fileSize = atoll(request[2]);
 			pthread_create(&thread[i+workNum*3], NULL, filerecv, (void*)argument); //실패 시 에러처리 추가
@@ -146,45 +146,47 @@ void* filerecv(void* arg) {
 	struct flock fifo_lock;
 	/*매개변수 저장*/
 	struct threadArg* argument = (struct threadArg*)arg;
-	sprintf(fifo2SerFileName, "%s2ser", argument->fifoFileName);
-	sprintf(fifo2CliFileName, "%s2cli", argument->fifoFileName);
-	sprintf(tempFileName, "%stemp.txt", argument->fifoFileName);
+	snprintf(fifo2SerFileName,FILENAMESIZE-1, "%s2ser", argument->fifoFileName);
+	snprintf(fifo2CliFileName,FILENAMESIZE-1, "%s2cli", argument->fifoFileName);
+	snprintf(tempFileName,FILENAMESIZE-1, "%stemp.txt", argument->fifoFileName);
 
 
 	/*3. 각 쓰레드는 3개의 "{요청자 PID}FIFO{n}2ser"와 "{요청자 PID}FIFO{n}2cli" FIFO 파일을 생성한다.  */
 	if (mkfifo(fifo2SerFileName, 0666) == -1) { //fifo init
 		printf("fail to make fifo 2ser() because :%d %s is not exist\n",errno,fifo2SerFileName);
-		error_handler("./managefifo", fifo2SerFileName, null, null);
+		exit(0);
 	}
 	if ((fifo2Ser = open(fifo2SerFileName, O_RDONLY | O_NONBLOCK)) < 0) {
 		printf("fail to call open manage()\n");
-		error_handler("./managefifo", fifo2SerFileName, null, null);
+		exit(0);
 	}
 
 	if (mkfifo(fifo2CliFileName, 0666) == -1) { //fifo init
 		printf("fail to make fifo 2cli()\n");
-		error_handler("./managefifo", fifo2SerFileName, fifo2CliFileName, null);
+		exit(0);
 	}
 	if ((fifo2Cli = open(fifo2CliFileName, O_WRONLY)) < 0) {
 		printf("fail to call open manage()\n");
-		error_handler("./managefifo", fifo2SerFileName, fifo2CliFileName, null);
+		exit(0);
 	}
 	/*6. server의 각 쓰레드는 "{요청자 PID}FIFO{n}" 파일을 읽고 복호화하여 "{요청자 PID}FIFO{n}temp.txt"파일에 임시 저장한다.*/
 
-	if ((tempfd = open(tempFileName, O_RDWR | O_CREAT)) < 0) {
+	if ((tempfd = open(tempFileName, O_RDWR | O_CREAT,0666)) < 0) {
 		printf("fail to call open manage()\n");
-		error_handler("./managefifo", fifo2SerFileName, fifo2CliFileName, tempFileName);
+		exit(0);
 	}
 
 	int readflag = 0;
+	int totallen = 0;
 	while (readflag == 0) {
 		while ((readlen = read(fifo2Ser, buf, BUF_SIZE)) > 0) {
 			decoding(buf, readlen);
 			write(tempfd, buf, readlen);
+			totallen += readlen;
 			readflag = 1;
 		}
 	}
-
+	
 	/* 7. server의 각 쓰레드는 "{요청자 PID}FIFO{n}temp.txt" 읽고 "{요청자 PID}FIFO{n}" 파일에 writelock를 건 후 write한다.*/
 	fifo_lock.l_type = F_WRLCK;
 	fifo_lock.l_whence = SEEK_SET;
@@ -194,30 +196,29 @@ void* filerecv(void* arg) {
 	if (fcntl(fifo2Cli, F_SETLKW, &fifo_lock) == -1) {
 		printf("fail to call fcntl(lock)\n");
 		write(fifo2Cli, "server error\n", 14);
-		error_handler("./managefifo", fifo2SerFileName, fifo2CliFileName, tempFileName);
+		exit(0);
 	}
-
-	lseek(tempfd, 0, SEEK_SET);
-	int totallen = 0;
+	
+	if(lseek(tempfd, 0, SEEK_SET)<0){
+		printf("lseek() error\n");
+		exit(0);
+	}
+	totallen = 0;
 	while ((readlen = read(tempfd, buf, BUF_SIZE)) > 0) {
 		write(fifo2Cli, buf, readlen);
 		totallen += readlen;
-
 	}
 
 	//fflush(fifo2Cli);
 	fifo_lock.l_type = F_UNLCK;
 	if (fcntl(fifo2Cli, F_SETLK, &fifo_lock) == -1) {
 		printf("fail to call fcntl(unlock)\n");
-		error_handler("./managefifo", fifo2SerFileName, fifo2CliFileName, tempFileName);
+		exit(0);
 	}
 
 
 	printf("Thread %d, send to %s: %d byte\n", argument->number,argument->fifoFileName,totallen);
-	//fflush(stdout);
-	unlink(fifo2SerFileName);
-	unlink(fifo2CliFileName);
-	unlink(tempFileName);
+	fflush(stdout);
 
 	close(fifo2Ser);
 	close(fifo2Cli);
